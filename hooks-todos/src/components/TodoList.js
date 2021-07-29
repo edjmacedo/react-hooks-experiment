0ff6f297bd4244999807318d24799870539f96cd
Edvan Macedo <edvan.macedo.jr@gmail.com>
2021-07-29 22:06:40 +0200

    Added tailwind style

--- hooks-todos/src/components/TodoList.js
+++ hooks-todos/src/components/TodoList.js
@@ -3,13 +3,32 @@ import TodosContext from '../context';
 
 export default function TodoList() {
     const { state } = useContext(TodosContext);
+    const title = state.todos.length > 0 ? `${state.todos.length} Todos` : 'Nothing to do';
 
     return (
-        <div>
-            <ul>
+        <div className="container mx-auto mx-w-md text-center font-mono">
+            <h1 className="text-bold">{title}</h1>
+            <ul className="list-reset p-0">
                 {state.todos.map((todo) => (
-                    <li key={todo.id}>
-                        <span>{todo.text}</span>
+                    <li
+                        key={todo.id}
+                        className="flex items-center bg-blue-100 border-black border-dashed border-2 my-2 py-4"
+                    >
+                        <span className="flex-1 ml-12 cursor-pointer">{todo.text}</span>
+                        <button>
+                            <img
+                                src="https://as2.ftcdn.net/v2/jpg/01/97/22/97/1000_F_197229786_vWEFpeQEOtIcjvtKVRAyPPP91ANs43uq.jpg"
+                                alt="Edit Icon"
+                                className="h-6"
+                            />
+                        </button>
+                        <button>
+                            <img
+                                src="https://as2.ftcdn.net/v2/jpg/03/46/38/39/1000_F_346383913_JQecl2DhpHy2YakDz1t3h0Tk3Ov8hikq.jpg"
+                                alt="Delete Icon"
+                                className="h-6"
+                            />
+                        </button>
                     </li>
                 ))}
             </ul>
